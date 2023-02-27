a351ad865c0619f0fc026b7c03e0e69e5ed9b9f2
suhail-ali1 <126316916+suhail-ali1@users.noreply.github.com>
2023-02-27 13:24:07 +0530

    1. First Java Code
    Java Hello World Program

---
+++ FirstJavaCode.java
@@ -0,0 +1,9 @@
+// This is My First Java Code
+public class FirstJavaCode { // Class which contains main method has to be public
+
+	public static void main(String[] args) { // main method has to be static so that JVM can call it without creating class Object.
+		
+		System.out.println("Hello World !");
+	}
+
+}
